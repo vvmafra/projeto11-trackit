cc10671770620833a016895c120996ba1278fbd2
Victor Mafra <victorvmafra97@gmail.com>
2023-03-15 18:49:20 -0300

    not finished layout yet :(

--- src/pages/Today.js
+++ src/pages/Today.js
@@ -4,21 +4,35 @@ import Bottom from "../components/Bottom"
 
 export default function Today() {
 
-    return(
+    return (
         <>
-        <Header/>
-        
-        <CoreToday>
-            <p>
-                Segunda, 15/03
-            </p>
-            <FinishedHabits>
-                Nenhum hábito concluído ainda
-            </FinishedHabits>
-        
-        </CoreToday>
+            <Header />
 
-        <Bottom/>
+            <CoreToday>
+                <p>Segunda, 15/03</p>
+
+                <FinishedHabits>
+                    Nenhum hábito concluído ainda
+                </FinishedHabits>
+
+                <ListHabits>
+
+                    <Habit>
+                        <Text>
+                            <p>Let 1 capitulo de livro</p>
+
+                            <span>Sequência atual: 4 dias</span>
+                            <span>Seu recorde: 3 dias</span>
+                        </Text>
+
+                        <ion-icon name="checkbox"></ion-icon>
+                    </Habit>
+
+                </ListHabits>
+
+            </CoreToday>
+
+            <Bottom />
         </>
     )
 }
@@ -40,9 +54,55 @@ const CoreToday = styled.div`
 `
 
 const FinishedHabits = styled.div`
+    font-family: 'Lexend Deca', sans-serif;
+    font-size: 18px;
+    color: #BABABA;
+    padding-top: 10px;
+    padding-left: 40px;
+`
+
+const ListHabits = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 25px;
+`
+
+const Habit = styled.div`
+    width: 340px;
+    height: 94px;
+    background: #FFFFFF;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px;
+    box-sizing: border-box;
+    
+    p {
         font-family: 'Lexend Deca', sans-serif;
         font-size: 18px;
-        color: #BABABA;
-        padding-top: 10px;
-        padding-left: 40px;
+        color: #666666;
+        font-size: 20px;
+        padding-top: 0px;
+        padding-left: 0px;
+        padding-bottom: 15px;
+    }
+
+    span {
+        font-family: 'Lexend Deca', sans-serif;
+        color: #666666;
+        font-size: 13px;
+    }
+
+    ion-icon {
+        width: 69px;
+        height: 69px;
+        color: #EBEBEB;
+    }
+`
+
+const Text = styled.div`
+    display: flex;
+    flex-direction: column;
 `
